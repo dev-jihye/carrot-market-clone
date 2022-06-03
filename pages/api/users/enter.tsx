ed45b1b4502deeb3e9d5a65925da401ccd541e93
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-04 00:22:02 +0900

    :see_no_evil: twilio 주석 처리

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -32,21 +32,21 @@ async function handler(
     },
   });
   if (phone) {
-    const message = await twilioClient.messages.create({
-      messagingServiceSid: process.env.TWILIO_MSID,
-      to: process.env.MY_PHONE!,
-      body: `Your login token is ${payload}`,
-    });
-    console.log(message);
+    // const message = await twilioClient.messages.create({
+    //   messagingServiceSid: process.env.TWILIO_MSID,
+    //   to: process.env.MY_PHONE!,
+    //   body: `Your login token is ${payload}`,
+    // });
+    // console.log(message);
   } else if (email) {
-    const email = await mail.send({
-      from: 'jihye_340@naver.com',
-      to: 'jihye_340@naver.com',
-      subject: 'Your Carrot Market Verification Mail',
-      text: `Your token is ${payload}`,
-      html: `<strong>Your token is ${payload}</strong>`,
-    });
-    console.log(email);
+    // const email = await mail.send({
+    //   from: 'jihye_340@naver.com',
+    //   to: 'jihye_340@naver.com',
+    //   subject: 'Your Carrot Market Verification Mail',
+    //   text: `Your token is ${payload}`,
+    //   html: `<strong>Your token is ${payload}</strong>`,
+    // });
+    // console.log(email);
   }
   return res.json({
     ok: true,
