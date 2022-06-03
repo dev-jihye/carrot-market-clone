4cd63cd4b9ab3cfb4a31d5226cb96c80398429c4
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-03 23:10:03 +0900

    :sparkles: sendgrid 메일 보내기 설정

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,9 +1,12 @@
+import mail from '@sendgrid/mail';
 import twilio from 'twilio';
 import client from '@libs/server/client';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
@@ -35,6 +38,15 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: 'jihye_340@naver.com',
+      to: 'jihye_340@naver.com',
+      subject: 'Your Carrot Market Verification Mail',
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
   }
   return res.json({
     ok: true,
