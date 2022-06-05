69b611c0ce2d1da835d8df171b083b78cb919442
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-05 22:19:43 +0900

    :necktie: session을 찾고 저장 후 token 지우기

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -9,17 +9,22 @@ async function handler(
 ) {
   console.log(req.session);
   const { token } = req.body;
-  const exists = await client.token.findUnique({
+  const foundToken = await client.token.findUnique({
     where: {
       payload: token,
     },
   });
-  if (!exists) return res.status(404).end();
+  if (!foundToken) return res.status(404).end();
   req.session.user = {
-    id: exists.userId,
+    id: foundToken.userId,
   };
   await req.session.save();
-  res.status(200).end();
+  await client.token.deleteMany({
+    where: {
+      userId: foundToken.userId,
+    },
+  });
+  res.json({ ok: true });
 }
 
 export default withApiSession(withHandler('POST', handler));
