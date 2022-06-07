b0f467e9a2ca3d8890eccea8f38c4905b416bd15
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-07 23:27:22 +0900

    ✨ products detail api 생성

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,32 @@
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: +id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avartar: true,
+        },
+      },
+    },
+  });
+  console.log(product);
+  res.json({
+    ok: true,
+    product,
+  });
+}
+
+export default withApiSession(withHandler({ methods: ['GET'], handler }));
