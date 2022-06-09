1d69d672397c8abacbaced5b0b0d6536de6e9ed0
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-09 15:57:57 +0900

    ✨ 궁금해요 mutate 추가

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -6,6 +6,8 @@ import useSWR from 'swr';
 import { Answer, Post, User } from '@prisma/client';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import useMutation from '@libs/client/useMutation';
+import { cls } from '@libs/client/utils';
 
 interface AnswerWithUser extends Answer {
   user: User;
@@ -23,13 +25,35 @@ interface PostWithUser extends Post {
 interface CommunityPostResponse {
   ok: boolean;
   post: PostWithUser;
+  isCuriosity: boolean;
 }
 
 const CommunityPostDetail: NextPage = () => {
   const router = useRouter();
-  const { data, error } = useSWR<CommunityPostResponse>(
+  const { data, mutate } = useSWR<CommunityPostResponse>(
     router.query.id ? `/api/posts/${router.query.id}` : null
   );
+  const [curiosity] = useMutation(`/api/posts/${router.query.id}/curiosity`);
+  const onCuriosityClick = () => {
+    if (!data) return;
+    mutate(
+      {
+        ...data,
+        post: {
+          ...data.post,
+          _count: {
+            ...data.post._count,
+            curiosity: data.isCuriosity
+              ? data.post._count.curiosity - 1
+              : data.post._count.curiosity + 1,
+          },
+        },
+        isCuriosity: !data.isCuriosity,
+      },
+      false
+    );
+    curiosity({});
+  };
 
   useEffect(() => {
     if (data && !data.ok) {
@@ -61,7 +85,13 @@ const CommunityPostDetail: NextPage = () => {
             {data?.post?.question}
           </div>
           <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t border-b-2 w-full">
-            <span className="flex space-x-2 items-center text-sm">
+            <button
+              onClick={onCuriosityClick}
+              className={cls(
+                'flex space-x-2 items-center text-sm',
+                data?.isCuriosity ? 'text-teal-600' : ''
+              )}
+            >
               <svg
                 className="w-4 h-4"
                 fill="none"
@@ -77,7 +107,7 @@ const CommunityPostDetail: NextPage = () => {
                 ></path>
               </svg>
               <span>궁금해요 {data?.post?._count?.curiosity}</span>
-            </span>
+            </button>
             <span className="flex space-x-2 items-center text-sm">
               <svg
                 className="w-4 h-4"
