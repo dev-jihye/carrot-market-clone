150bb6aad911213c5975e6b52c7a2e765e7437de
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-09 12:52:14 +0900

    ✨ react-hook-form 적용하기

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -2,12 +2,25 @@ import type { NextPage } from 'next';
 import Button from '@components/button';
 import Layout from '@components/layout';
 import Textarea from '@components/textarea';
+import { useForm } from 'react-hook-form';
+
+interface WriteForm {
+  question: string;
+}
 
 const Write: NextPage = () => {
+  const { register, handleSubmit } = useForm<WriteForm>();
+  const onValid = (data: WriteForm) => {
+    console.log(data);
+  };
   return (
     <Layout canGoBack title="Write Post">
-      <form className="p-4 space-y-4">
-        <Textarea required placeholder="Ask a question!" />
+      <form onSubmit={handleSubmit(onValid)} className="p-4 space-y-4">
+        <Textarea
+          register={register('question', { required: true, minLength: 5 })}
+          required
+          placeholder="Ask a question!"
+        />
         <Button text="Submit" />
       </form>
     </Layout>
