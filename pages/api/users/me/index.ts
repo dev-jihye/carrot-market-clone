b88a7c49e190557ca87ceaae82155fef1890c4ec
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-10 23:56:08 +0900

    ✨ profile api POST 요청 추가

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -10,11 +10,67 @@ async function handler(
   const profile = await client.user.findUnique({
     where: { id: req.session.user?.id },
   });
-  if (!profile) {
-    res.status(404).json({
-      ok: false,
-      error: 'Page not found',
-    });
+  if (req.method === 'GET') {
+    if (!profile) {
+      res.status(404).json({
+        ok: false,
+        error: 'Page not found',
+      });
+    }
+  }
+  if (req.method === 'POST') {
+    const {
+      session: { user },
+      body: { email, phone },
+    } = req;
+    if (email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: { email },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: 'Email already exists',
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          email,
+        },
+      });
+      res.json({ ok: true });
+    } else if (phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: { phone },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({
+          ok: false,
+          error: 'Phone number already exists',
+        });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          phone,
+        },
+      });
+    }
   }
   res.json({
     ok: true,
@@ -22,4 +78,6 @@ async function handler(
   });
 }
 
-export default withApiSession(withHandler({ methods: ['GET'], handler }));
+export default withApiSession(
+  withHandler({ methods: ['GET', 'POST'], handler })
+);
