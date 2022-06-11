f48f29e4e5e418eba30b33ed5172642289df2453
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-11 18:33:19 +0900

    ✨ api/streams/[id]/messages에 useMutation 요청

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { Stream } from '@prisma/client';
 import { useForm } from 'react-hook-form';
+import useMutation from '@libs/client/useMutation';
 
 interface StreamResponse {
   ok: true;
@@ -21,9 +22,13 @@ const Stream: NextPage = () => {
   const { data } = useSWR<StreamResponse>(
     router.query.id ? `/api/streams/${router.query.id}` : null
   );
-
+  const [sendMessage, { loading, data: sendMessageData }] = useMutation(
+    `/api/streams/${router.query.id}/messages`
+  );
   const onValid = (form: MessageForm) => {
+    if (loading) return;
     reset();
+    sendMessage(form);
   };
   return (
     <Layout canGoBack>
@@ -61,19 +66,6 @@ const Stream: NextPage = () => {
                 </button>
               </div>
             </form>
-          </div>
-        </div>
-        <div className="fixed py-2 bg-white bottom-0 inset-x-0">
-          <div className="flex relative max-w-md items-center w-full mx-auto">
-            <input
-              type="text"
-              className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
-            />
-            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
-              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
-                &rarr;
-              </button>
-            </div>
           </div>
         </div>
       </div>
