a189337b3ac34afc135506211e8d5860257db62e
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-18 23:50:56 +0900

    ✨ 프로필 페이지 아바타 정보 받아오기

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -23,7 +23,14 @@ const Profile: NextPage = () => {
     <Layout title="나의 캐럿" hasTabBar>
       <div className="py-10 px-4">
         <div className="flex items-center space-x-3">
-          <div className="w-16 h-16 bg-slate-500 rounded-full" />
+          {user?.avatar ? (
+            <img
+              src={`https://imagedelivery.net/F8_vw_59rp9m4aU4WbbrxA/${user?.avatar}/public`}
+              className="w-16 h-16 rounded-full"
+            />
+          ) : (
+            <div className="w-16 h-16 bg-slate-500 rounded-full" />
+          )}
           <div className="flex flex-col">
             <span className="font-medium text-gray-900">{user?.name}</span>
             <Link href="/profile/edit">
