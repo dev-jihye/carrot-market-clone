1a5d438e138e5512ec7ea40729b30431f15982ae
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2022-06-19 00:49:39 +0900

    ✨ 상품페이지 이미지 데이터 받아오기

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -35,9 +35,15 @@ const ItemDetail: NextPage = () => {
     <Layout canGoBack>
       <div className="px-4 py-4">
         <div className="mb-8">
-          <div className="h-96 bg-slate-300" />
+          <img
+            src={`https://imagedelivery.net/F8_vw_59rp9m4aU4WbbrxA/${data?.product.image}/public`}
+            className="h-96 bg-slate-300 mx-auto"
+          />
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <img
+              src={`https://imagedelivery.net/F8_vw_59rp9m4aU4WbbrxA/${data?.product?.user?.avatar}/avatar`}
+              className="w-12 h-12 rounded-full bg-slate-300"
+            />
             <div>
               <p className="text-sm font-medium text-gray-700">
                 {data?.product?.user?.name}
